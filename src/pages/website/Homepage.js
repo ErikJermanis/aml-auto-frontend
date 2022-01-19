e2569bdd4735619723a7f8e0eb690dab38a06772
Dominik Kinkela <dominikkinkela@gmail.com>
2022-01-19 19:43:41 +0100

    Zamijenjen lorem ipsum, dodana ikona i naziv, spojen Book a car button

--- src/pages/website/Homepage.js
+++ src/pages/website/Homepage.js
@@ -17,18 +17,18 @@ const Homepage = () => {
     <div className="h-screen pt-16 flex items-center justify-evenly">
       <div className="w-2/5">
         <h1 className="text-4xl font-bold bg-orange-500">Easy and fast way to rent your next car</h1>
-        <p className="my-9">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam arcu tellus, accumsan sed elit iaculis, pulvinar feugiat metus. Nam sed diam purus. Morbi at gravida nisi, quis lobortis massa.</p>
+        <p className="my-9">We offer premium services and cars to ensure that you enjoy your drive in Croatia with comfort and style. We offer a wide choice of vehicles at affordable prices, as well as the useful additional services which will make your journey more simple and interesting.</p>
         <div>
-          <Button primary className="mr-4">Book a car</Button>
+          <Link to="book"><Button primary className="mr-4">Book a car</Button></Link>
           <Link to="cars"><Button secondary>Browse fleet</Button></Link>
         </div>
       </div>
       <CarsSvg className="w-1/5" />
     </div>
     <div className='py-32 flex justify-center items-start'>
-      <HPcard title="Fast & easy booking">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam arcu tellus, accumsan sed elit iaculis, pulvinar feugiat metus. Nam sed diam purus. Morbi at gravida nisi, quis lobortis massa.</HPcard>
-      <HPcard title="Many pickup locations">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam arcu tellus, accumsan sed elit iaculis, pulvinar feugiat metus. Nam sed diam purus. Morbi at gravida nisi, quis lobortis massa. Vivamus sapien turpis, vehicula sed ligula eu, suscipit iaculis nunc. Phasellus suscipit efficitur odio eget dapibus. Sed convallis posuere justo.</HPcard>
-      <HPcard title="Satisfied customers">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam arcu tellus, accumsan sed elit iaculis, pulvinar feugiat metus. Nam sed diam purus. Morbi at gravida nisi, quis lobortis massa. Vivamus sapien turpis, vehicula sed ligula eu, suscipit iaculis nunc. Phasellus suscipit efficitur odio eget dapibus. Sed convallis posuere justo. Vestibulum vulputate egestas quam in sollicitudin. Mauris interdum orci sem. Praesent vitae magna nec turpis pharetra maximus. Ut ac justo vestibulum, ultrices urna et, aliquam nulla.</HPcard>
+      <HPcard title="Fast & easy booking">We strive to make the car rental process at AML Auto pleasurable allowing you to make your reservations swiftly through online booking.</HPcard>
+      <HPcard title="Many pickup locations">Pick-up your rental car at one of the many AML Auto locations throughout Croatia. Explore the nearby area or venture out to different cities or even countries and drop the car off at a place of your choosing.</HPcard>
+      <HPcard title="Satisfied customers">With more than 2000 satisfied customers we are sure we will be the right ones for you.</HPcard>
     </div>
     </>
   )
